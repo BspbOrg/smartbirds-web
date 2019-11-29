ab51b6ecb7f3e44a99d1a89bc6725edb42fba8bb
Geno Roupsky <geno@roupsky.name>
2019-11-29 16:52:18 +0200

    Sample cypress test (#9)
     - Configure cypress tests
     - Setup github workflow for running cypress tests on push

---
+++ cypress/integration/demo.spec.js
@@ -0,0 +1,13 @@
+/* global context, beforeEach, cy, it */
+
+context('Demo', () => {
+  context('Visit the homepage', () => {
+    beforeEach(() => {
+      cy.visit('/')
+    })
+
+    it('display copyright', () => {
+      cy.get('body').contains('Copyright ©')
+    })
+  })
+})
