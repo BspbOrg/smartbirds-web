384ec5055f3569434047c390ecd243394bb8c74a
Geno Roupsky <geno@roupsky.name>
2020-02-01 15:47:40 +0200

    Use API_PREFIX if available for ENDPOINT_URL

--- client/scripts/configs/endpoint.js
+++ client/scripts/configs/endpoint.js
@@ -1,12 +1,9 @@
 var app = require('../app')
 
-switch (process.env.NODE_ENV || 'development') {
-  case 'development':
-    app.constant('ENDPOINT_URL', 'http://' + window.location.hostname + ':5000/api')
-    break
-  default:
-    app.constant('ENDPOINT_URL', '/api')
-    break
-}
+app.constant('ENDPOINT_URL', process.env.API_PREFIX || (
+  (process.env.NODE_ENV || 'development') === 'development'
+    ? 'http://' + window.location.hostname + ':5000/api'
+    : '/api'
+))
 
 app.constant('BANNER_BASE_URL', window.location.protocol + '//' + window.location.hostname + (window.location.port && ':' + window.location.port) + '/banner/')
