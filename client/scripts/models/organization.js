67ef19830bcfaeee92c509b00c485ee822b2b3ac
Geno Roupsky <geno@roupsky.name>
2020-04-17 12:47:22 +0300

    Use getLocalLabel in places that were duplicating the logic

--- client/scripts/models/organization.js
+++ client/scripts/models/organization.js
@@ -1,18 +1,15 @@
 var angular = require('angular')
 
-require('../app').factory('Organization', /* @ngInject */function ($resource, ENDPOINT_URL, $translate) {
+require('../app').factory('Organization', /* @ngInject */function ($resource, ENDPOINT_URL, $translate, localization) {
   var Organization = $resource(ENDPOINT_URL + '/organization/:slug', {
     slug: '@slug'
   })
 
   // methods
   angular.extend(Organization.prototype, {
-
     toString: function (locale) {
-      locale = locale || $translate.$language || 'en'
-      return (this.label || {})[locale]
+      return localization.getLocalLabel(this.label, locale)
     }
-
   })
 
   return Organization
