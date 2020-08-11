def96b46af84f234535ff7be6e621944b30be1f2
Geno Roupsky <geno@roupsky.name>
2020-08-11 22:44:39 +0300

    feat(bgatlas): display species for a cell

--- client/scripts/atlas/controllers/AtlasDashboardController.js
+++ client/scripts/atlas/controllers/AtlasDashboardController.js
@@ -7,7 +7,54 @@ module.exports = /* @ngInject */function AtlasDashboardController (api, ngToast,
 
   BaseMapController.apply(this)
 
+  $ctrl.selected = null
+  $ctrl.rows = []
+  $ctrl.loading = false
+
   $ctrl.cells = (user.getIdentity().bgatlasCells || []).map(function (cell) {
     return utils.mapCellToMapModel(cell)
   })
+
+  var lastCellInfoRequest = null
+
+  $ctrl.events = {
+    click: function (poly, event, model) {
+      if (lastCellInfoRequest) {
+        lastCellInfoRequest.cancel()
+        lastCellInfoRequest = null
+      }
+      if ($ctrl.selected === model) {
+        $ctrl.loading = false
+        $ctrl.selected = null
+        $ctrl.rows = []
+      } else {
+        $ctrl.selected = model
+        $ctrl.rows = []
+        $ctrl.loading = true
+        var call = lastCellInfoRequest = api.bgatlas2008.getCellInfo(model.cell.utm_code)
+        call
+          .then(function (rows) {
+            if ($ctrl.selected === model) {
+              $ctrl.rows = rows
+            }
+          })
+          .catch(function (error) {
+            if (error && error.status !== -1) {
+              ngToast.create({
+                className: 'danger',
+                content: '<p>' + $translate.instant('Error while loading') + '</p><pre>' + (error && error.data ? error.data.error : JSON.stringify(error, null, 2)) + '</pre>'
+              })
+            }
+          })
+          .finally(function () {
+            if ($ctrl.selected === model) {
+              $ctrl.loading = false
+            }
+            if (call === lastCellInfoRequest) {
+              lastCellInfoRequest = null
+            }
+          })
+      }
+    }
+  }
 }
