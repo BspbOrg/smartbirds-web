841908877173dcbed1bc30f77f508dba26dc3511
Geno Roupsky <geno@roupsky.name>
2021-02-02 10:27:14 +0200

    Select language to display in species editor (#238)

--- client/scripts/controllers/SpeciesController.js
+++ client/scripts/controllers/SpeciesController.js
@@ -1,10 +1,31 @@
-var languages = require('../../../config/languages')
-var angular = require('angular')
-require('../app').controller('SpeciesController', /* @ngInject */function ($scope, $state, $stateParams, db, Species) {
-  var $ctrl = this
+const languages = require('../../../config/languages')
+const angular = require('angular')
+require('../app').controller('SpeciesController', /* @ngInject */function (
+  db, $scope, Species, $state, $stateParams, $translate) {
+  const $ctrl = this
 
   $ctrl.species = db.species
+
+  $ctrl.currentLanguage = $translate.$language || 'en'
   $ctrl.languages = languages
+  $ctrl.visibleLanguages = { en: languages.en }
+  $ctrl.visibleLanguages[$ctrl.currentLanguage] = languages[$ctrl.currentLanguage]
+  $ctrl.languageSelectorIsOpen = false
+  $ctrl.setCurrentLanguage = function (key, $event) {
+    if ($event) {
+      $event.preventDefault()
+      $event.stopPropagation()
+    }
+    $ctrl.languageSelectorIsOpen = false
+    $ctrl.currentLanguage = key
+    if (key) {
+      $ctrl.visibleLanguages = { en: languages.en }
+      $ctrl.visibleLanguages[$ctrl.currentLanguage] = languages[$ctrl.currentLanguage]
+    } else {
+      $ctrl.visibleLanguages = languages
+    }
+  }
+
   if ($stateParams.type && $stateParams.type in db.species) {
     $ctrl.selected = $ctrl.selected || {}
     $ctrl.selected.type = $stateParams.type
