b0df46a935304d9edbe8b2f0114dd5c7bf5cd1b0
Geno Roupsky <geno@roupsky.name>
2022-10-08 11:48:28 +0300

    feat(build): generate fonticon from svg icons

---
+++ .fantasticonrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  inputDir: './client/assets/icons',
+  outputDir: './public/fonts',
+  fontTypes: ['eot', 'svg', 'ttf', 'woff', 'woff2'],
+  assetTypes: ['css'],
+  fontsUrl: '/fonts',
+  pathOptions: {
+    css: './public/css/icons.css',
+  },
+};
