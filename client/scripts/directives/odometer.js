d038c55361b184654a159cce3bd7ca8ae859b4c8
Daniel Petrov <dani@code6.ninja>
2023-05-15 16:37:21 +0300

    feat(counter): enable records counter on public home page to count 2M record

--- client/scripts/directives/odometer.js
+++ client/scripts/directives/odometer.js
@@ -1,20 +1,20 @@
-var Odometer = require('odometer')
+const Odometer = require('odometer')
 
 require('../app').directive('sbOdometer', /* @ngInject */function () {
   return {
     restrict: 'AE',
     scope: { value: '=' },
     link: function (scope, element) {
-      var odometer = new Odometer({
+      const odometer = new Odometer({
         el: element[0],
         value: 0,
-        format: 'ddd,dddd',
+        format: 'dd,ddd,dddd',
         duration: 2000,
         animation: 'count'
       })
 
       scope.$watch('value', function (value) {
-        odometer.update(Math.min(parseInt(value), 999999) * 10)
+        odometer.update(parseInt(value) * 10)
       })
     }
   }
