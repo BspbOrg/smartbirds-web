dc54cfe99f08f725455ae8c7a4fcd3dba7404d95
Daniel Petrov <dani@code6.ninja>
2024-12-17 17:21:06 +0200

    fixup! WIP add ebp settings page

--- client/scripts/controllers/EBPSettingsController.js
+++ client/scripts/controllers/EBPSettingsController.js
@@ -1,17 +1,8 @@
-require('../app').controller('EBPSettingsController', /* @ngInject */function ($q, $stateParams, EBPSpecies) {
+require('../app').controller('EBPSettingsController', /* @ngInject */function ($scope, $q, $stateParams, EBPSpecies) {
   const $ctrl = this
 
   $ctrl.selected = 'sources'
-
   if ($stateParams.settings) {
     $ctrl.selected = $stateParams.settings
   }
-
-  $ctrl.requestSpecies = function () {
-    $q.resolve(EBPSpecies.query().$promise).then(function (species) {
-      $ctrl.species = species
-    })
-  }
-
-  $ctrl.requestSpecies()
 })
