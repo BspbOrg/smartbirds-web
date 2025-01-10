38005f043ffcbe7be74df99c5d98ec59b5b35c65
Daniel Petrov <dani@code6.ninja>
2025-01-10 12:07:30 +0200

    Update api paths

--- client/scripts/models/ebp_species.js
+++ client/scripts/models/ebp_species.js
@@ -1,5 +1,5 @@
 require('../app').factory('EBPSpecies', /* @ngInject */function ($resource, ENDPOINT_URL, localization) {
-  const EBPSpecies = $resource(ENDPOINT_URL + '/ebp-species', {}, {
+  const EBPSpecies = $resource(ENDPOINT_URL + '/ebp/species', {}, {
     update: { method: 'PUT', isArray: true }
   })
   return EBPSpecies
--- client/scripts/models/ebp_species_status.js
+++ client/scripts/models/ebp_species_status.js
@@ -1,5 +1,5 @@
 require('../app').factory('EBPSpeciesStatus', /* @ngInject */function ($resource, ENDPOINT_URL, localization) {
-  const EBPSpeciesStatus = $resource(ENDPOINT_URL + '/ebp-species-status', {}, {
+  const EBPSpeciesStatus = $resource(ENDPOINT_URL + '/ebp/species-status', {}, {
     update: { method: 'PUT', isArray: true }
   })
   return EBPSpeciesStatus
