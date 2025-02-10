2500aa46f537dfe702500d8c6cb9dcc05fb28d78
Daniel Petrov <dani@code6.ninja>
2025-02-10 17:57:10 +0200

    fixup! WIP: read server config

--- client/scripts/controllers/ServerConfigController.js
+++ client/scripts/controllers/ServerConfigController.js
@@ -1,11 +1,15 @@
 require('../app').controller('ServerConfigController', /* @ngInject */function (api) {
   const $ctrl = this
   $ctrl.serverConfig = 'N/A'
+  $ctrl.configVariables = []
 
   $ctrl.requestConfig = function () {
     api.server.config().then(function (res) {
-      $ctrl.serverConfig = res.config || ''
+      $ctrl.configVariables = (res.config.environmentVariables || []).sort((a, b) => a.key.localeCompare(b.key))
     })
+  }
+
+  $ctrl.save = () => {
   }
 
   $ctrl.requestConfig()
